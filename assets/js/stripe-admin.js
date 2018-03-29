c917bf3bedefbbd16545a9f88fa4143474cb0107
websnail <info@snailsolutions.co.uk>
2018-03-30 00:59:12 +0100

    Initial commit - Applied hack to get settings fields generated and saved for selected stripe currency accounts - Updated Jquery code that hides/displays fields for Stripe API key fields to work with the new currency fields
    It's all untested and probably broken

--- assets/js/stripe-admin.js
+++ assets/js/stripe-admin.js
@@ -1,6 +1,11 @@
 jQuery( function( $ ) {
 	'use strict';
 
+	/*
+	 * Multi-currency MOD
+	 */
+	var wc_stripe_accepted_currencies = array('gbp','usd','eur');
+
 	/**
 	 * Object to handle Stripe admin functions.
 	 */
@@ -22,22 +27,35 @@ jQuery( function( $ ) {
 		 */
 		init: function() {
 			$( document.body ).on( 'change', '#woocommerce_stripe_testmode', function() {
-				var test_secret_key = $( '#woocommerce_stripe_test_secret_key' ).parents( 'tr' ).eq( 0 ),
-					test_publishable_key = $( '#woocommerce_stripe_test_publishable_key' ).parents( 'tr' ).eq( 0 ),
-					live_secret_key = $( '#woocommerce_stripe_secret_key' ).parents( 'tr' ).eq( 0 ),
-					live_publishable_key = $( '#woocommerce_stripe_publishable_key' ).parents( 'tr' ).eq( 0 );
 
-				if ( $( this ).is( ':checked' ) ) {
-					test_secret_key.show();
-					test_publishable_key.show();
-					live_secret_key.hide();
-					live_publishable_key.hide();
-				} else {
-					test_secret_key.hide();
-					test_publishable_key.hide();
-					live_secret_key.show();
-					live_publishable_key.show();
-				}
+				$.each(wc_stripe_accepted_currencies, function(index, currency) {
+
+					var label = '#woocommerce_stripe_'+currency;
+
+					/*
+					 * Skip these keys if the [currency]_test_secret_key field doesn't exist
+					 */
+					if( !$((label+'_test_secret_key')).length ) {
+						return;
+					}
+
+					var test_secret_key = $( (label+'_test_secret_key') ).parents( 'tr' ).eq( 0 ),
+						test_publishable_key = $( (label+'_test_publishable_key') ).parents( 'tr' ).eq( 0 ),
+						live_secret_key = $( (label+'_secret_key') ).parents( 'tr' ).eq( 0 ),
+						live_publishable_key = $( (label+'_publishable_key') ).parents( 'tr' ).eq( 0 );
+
+					if ( $( this ).is( ':checked' ) ) {
+						test_secret_key.show();
+						test_publishable_key.show();
+						live_secret_key.hide();
+						live_publishable_key.hide();
+					} else {
+						test_secret_key.hide();
+						test_publishable_key.hide();
+						live_secret_key.show();
+						live_publishable_key.show();
+					}
+				} );
 			} );
 
 			$( '#woocommerce_stripe_testmode' ).change();
